1a7b1d01fb1e32ee50cf356044303dc99d96293e
Thijs Spijker <thijs.spijker@hva.nl>
2020-02-04 11:53:00 +0100

    add fetch utilities

---
+++ scripts/modules/fetch-utilities.js
@@ -0,0 +1,38 @@
+/* 
+ * Module to append fetch with some additional modules
+ * based on https://codeburst.io/fetch-api-was-bringing-darkness-to-my-codebase-so-i-did-something-to-illuminate-it-7f2d8826e939
+ */
+
+
+/**
+ * Checks if the response is 'ok'
+ * @param {*} response - the response object from a fetch request
+ * @returns {Promise<*>} if response is ok, resolves with the response. Else rejects with an error
+ */
+const checkStatus = response => {
+	if (response.ok) return response;
+	else {
+		const error = new Error(response.statusText);
+		error.response = response;
+		throw error;
+	}
+};
+
+/**
+ * Parses a response to JSON
+ * @param {*} response - the response object from a fetch request
+ * @returns {Promise<*>} the parsed response object
+ */
+const parseJSON = res => res.json();
+
+/**
+ * Fetch with added utilities like check for status code and json parse
+ * @param {string} url - the url for this get request
+ * @param {*} [init] - An object containing any custom settings that you want to apply to the request
+ * @returns {Promise<*>} The resolved JSON parsed response if 200 Ok or rejection with the error reason
+ */
+export function get(url, init) {
+	return fetch(url, init)
+		.then(checkStatus)
+		.then(parseJSON);
+}
