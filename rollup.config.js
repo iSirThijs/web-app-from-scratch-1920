82e041dbe736910e68d5193b0a9bc7c989f8a5d9
Thijs Spijker <thijs.spijker@hva.nl>
2020-02-24 19:56:34 +0100

    add link alias to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
+import alias from '@rollup/plugin-alias'
+
 export default {
-	input: 'src/app.mjs',
+	input: 'src/router.mjs',
 	output: {
-		file: 'docs/scripts/app.js',
+		file: 'docs/scripts/main.js',
 		format: 'umd',
 		sourcemap: true
 	},
 	watch: {
 		exclude: 'node_modules/**'
-	}
+	},
+	plugins: [
+		alias({
+			entries: {
+				utils: 'src/utilities',
+				modules: 'src/modules',
+				components: 'src/components',
+				pages: 'src/pages'
+			}
+		})
+	]
 }
